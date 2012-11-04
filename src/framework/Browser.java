371a0619543de09ec0c3415997632738c8848f75
Max Krutskikh <maximkrut@yahoo.com>
2012-11-04 13:25:11 +0200

    From Eclipse

--- src/framework/Browser.java
+++ src/framework/Browser.java
@@ -10,7 +10,7 @@ package framework;
 public class Browser {
 
 	/**
-	 * @param args
+	 * @param args alll parameters
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
